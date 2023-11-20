2bf520bc0eba2656af67ee5067fba926bc584fcc
Juan Casado Ballesteros <jcb7777777@gmail.com>
2023-11-20 19:56:02 +0100

    Initial comit - UI basic layout

---
+++ main.js
@@ -0,0 +1,57 @@
+
+const started = Date.now()
+
+let toDo = []
+let onProgress = null
+let done = []
+
+class Item {
+    constructor(text) {
+        this.text = text
+        this.id = crypto.randomUUID()
+    }
+}
+
+class OnProgressItem {
+    constructor(item) {
+        this.item = item
+        this.started = Date.now()
+    }
+
+    done() {
+        return DoneItem(this.item)
+    }
+    
+    toDo() {
+        return TodoItem(this.item)
+    }
+}
+
+class TodoItem {
+    constructor(item) {
+        this.item = item
+    }
+
+    done() {
+        return DoneItem(this.item)
+    }
+
+    start() {
+        return OnProgressItem(this)
+    }
+}
+
+class DoneItem {
+    constructor(item) {
+        this.item = item
+        this.done = Date.now()
+    }
+
+    toDo() {
+        return TodoItem(this.item)
+    }
+
+    start() {
+        return OnProgressItem(this)
+    }
+}
